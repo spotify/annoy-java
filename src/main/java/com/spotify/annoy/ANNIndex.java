aec9fcc8620ebff8b0b2976183c5a82a0532488c
Andy Sloane <as@spotify.com>
2014-02-25 13:34:47 -0600

    Initial commit; base functionality exists
    Change-Id: I76c0e951aa8eb5e46ed024d5c10887cee9598676

---
+++ src/main/java/com/spotify/annoy/ANNIndex.java
@@ -0,0 +1,151 @@
+package com.spotify.annoy;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.*;
+
+public class ANNIndex {
+
+  ANNIndex(int dimension, String filename) throws IOException {
+    init(dimension);
+    load(filename);
+  }
+
+  private void init(int dimension) {
+    this.dimension = dimension;
+    roots = new ArrayList<Integer>();
+    // we can store up to minLeafSize children in leaf nodes (we put them where the separating
+    // plane normally goes)
+    this.minLeafSize = dimension + 2;
+    this.nodeSize = 12 + 4*dimension;
+  }
+
+  private void load(String filename) throws IOException {
+    memoryMappedFile = new RandomAccessFile(filename, "r");
+    int fileSize = (int) memoryMappedFile.length();
+    System.err.printf("%s: %d bytes\n", filename, fileSize);
+
+    // We only support indexes <4GB as a result of ByteBuffer using an int index
+    annBuf = memoryMappedFile.getChannel().map(
+            FileChannel.MapMode.READ_ONLY, 0, fileSize);
+    annBuf.order(ByteOrder.LITTLE_ENDIAN);
+
+    // an Angular::node contains:
+    // 4   int n_descendants
+    // 8   int children[2]
+    // --  float v[dimension]
+    // 12 + 4*dimension total
+    int m = -1;
+    for (int i = fileSize - nodeSize; i >= 0; i -= nodeSize) {
+      int k = annBuf.getInt(i);  // node[i].n_descendants
+      if (m == -1 || k == m) {
+        roots.add(i);
+        m = k;
+      } else {
+        break;
+      }
+    }
+
+    System.err.printf("%s: found %d roots with degree %d\n",
+                      filename, roots.size(), m);
+  }
+
+  public void getNodeVector(int node, float[] v) {
+    for (int i = 0; i < dimension; i++) {
+      v[i] = annBuf.getFloat(i*4 + node + 12);
+    }
+  }
+
+  public void getItemVector(int item, float[] v) {
+    getNodeVector(item * nodeSize, v);
+  }
+
+  public static float margin(float[] u, float[] v) {
+    float d = 0;
+    for (int i = 0; i < u.length; i++) {
+      d += u[i] * v[i];
+    }
+    return d;
+  }
+
+  private class PQEntry implements Comparable<PQEntry> {
+    PQEntry(float margin, int node) { this.margin = margin; this.node = node; }
+    public float margin;
+    public int node;
+
+    @Override
+    public int compareTo(PQEntry o) {
+      if (o.margin == margin) return 0;
+      return o.margin > margin ? 1 : -1;
+    }
+  }
+
+  public List<Integer> getNearest(float[] queryVector, int nResults) {
+    PriorityQueue<PQEntry> pq = new PriorityQueue<PQEntry>(
+            roots.size()*4);
+    float[] v = new float[dimension];
+    for (int r : roots) {
+      getNodeVector(r, v);
+      pq.add(new PQEntry(1e30f, r));
+    }
+
+    Set<Integer> NNs = new HashSet<Integer>();
+    while (NNs.size() < roots.size() * nResults && !pq.isEmpty()) {
+      PQEntry top = pq.poll();
+      int n = top.node;
+      int nDescendants = annBuf.getInt(n);
+      if (nDescendants == 1) {  // n_descendants
+        // FIXME: does this ever happen?
+        NNs.add(n / nodeSize);
+      } else if (nDescendants <= minLeafSize) {
+        for (int i = 0; i < nDescendants; i++) {
+          int j = annBuf.getInt(n + 4 + i*4);
+          NNs.add(j);
+        }
+      } else {
+        getNodeVector(n, v);
+        float margin = margin(v, queryVector);
+        int lChild = nodeSize * annBuf.getInt(n+4);
+        int rChild = nodeSize * annBuf.getInt(n+8);
+        pq.add(new PQEntry(-margin, lChild));
+        pq.add(new PQEntry( margin, rChild));
+      }
+    }
+    PQEntry sortedNNs[] = new PQEntry[NNs.size()];
+    int i = 0;
+    for (int nn : NNs) {
+      getItemVector(nn, v);
+      sortedNNs[i++] = new PQEntry(margin(v, queryVector), nn);
+    }
+    Arrays.sort(sortedNNs);
+    ArrayList<Integer> result = new ArrayList<Integer>(nResults);
+    for (i = 0; i < nResults && i < sortedNNs.length; i++)
+      result.add(sortedNNs[i].node);
+    return result;
+  }
+
+  public static void main(String[] args) throws Exception {
+    ANNIndex annIndex = new ANNIndex(Integer.parseInt(args[0]), args[1]);
+    float[] u = new float[annIndex.dimension],
+            v = new float[annIndex.dimension];
+    int queryItem = Integer.parseInt(args[2]);
+    annIndex.getItemVector(queryItem, u);
+    System.out.printf("vector[%d]: ", queryItem);
+    for (float x : u)
+      System.out.printf("%2.2f ", x);
+    System.out.printf("\n");
+    List<Integer> NNs =  annIndex.getNearest(u, 10);
+    for (int nn : NNs) {
+      annIndex.getItemVector(nn, v);
+      System.out.printf("%d %d %f\n", queryItem, nn, margin(u, v));
+    }
+  }
+
+  public int dimension, minLeafSize, nodeSize;
+  private ArrayList<Integer> roots;
+  private RandomAccessFile memoryMappedFile;
+  private MappedByteBuffer annBuf;
+}
---
+++ src/test/java/com/spotify/annoy/ANNIndexTest.java
@@ -0,0 +1,66 @@
+package com.spotify.annoy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(JUnit4.class)
+public class ANNIndexTest {
+  @Test
+  public void testSampleIndex() {
+    try {
+      ANNIndex annIndex = new ANNIndex(5, "src/test/resources/test-index.tree");
+      float u[] = new float[5],
+            v[] = new float[5];
+      annIndex.getItemVector(0, u);
+      List<Integer> NNs = annIndex.getNearest(u, 10);
+      // there are 100 items in the test index, so we should definitely fill our request for 10
+      assertEquals(NNs.size(), 10);
+      // the most similar item should be the item we started with
+      assertEquals((int) NNs.get(0), 0);
+      // just for fun, print 'em
+      for (int nn : NNs) {
+        annIndex.getItemVector(nn, v);
+        System.out.printf("%d %f\n", nn, ANNIndex.margin(u, v));
+      }
+    } catch (Exception e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+
+  private void normalize(float[] vec) {
+    double mag = 1.0 / Math.sqrt(ANNIndex.margin(vec, vec));
+    for (int i = 0; i < vec.length; i++)
+      vec[i] *= mag;
+  }
+
+  public void benchmarkAccuracy() {
+    try {
+      ANNIndex annIndex = new ANNIndex(40, "vector_exp-artist-40.tree");
+      float[] v = new float[40],
+              u = new float[40];
+      float accuracysum = 0;
+      for (int i = 0; i < 100000; i += 100) {
+        annIndex.getItemVector(i, v);
+        normalize(v);
+        List<Integer> NNs = annIndex.getNearest(v, 100);
+        float marginsum = 0;
+        for (int nn : NNs) {
+          annIndex.getItemVector(nn, u);
+          normalize(u);
+          marginsum += ANNIndex.margin(u, v);
+        }
+        accuracysum += marginsum / NNs.size();
+      }
+      System.err.printf("avg cosine dist: %f\n", accuracysum / 1000);
+    } catch (IOException e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+}

f42d5805915ef88b847168b7f4d47c95befea9a9
Neville Li <neville@spotify.com>
2019-08-12 14:35:09 -0400

    fix javadoc

--- src/main/java/com/spotify/annoy/ANNIndex.java
+++ src/main/java/com/spotify/annoy/ANNIndex.java
@@ -195,7 +195,7 @@ public class ANNIndex implements AnnoyIndex {
    * Closes this stream and releases any system resources associated
    * with it. If the stream is already closed then invoking this
    * method has no effect.
-   * <p/>
+   *
    * <p> As noted in {@link AutoCloseable#close()}, cases where the
    * close may fail require careful attention. It is strongly advised
    * to relinquish the underlying resources and to internally
